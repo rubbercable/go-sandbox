5a9f442bef4f1725337e6714d03bf3ddc3eaf844
rubbercable <rubbercable@gmail.com>
2022-04-03 22:11:00 +0100

    crud1

---
+++ 03_crud_api/src/go-movies-crud/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbm     string    `json:"isbm"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	movie.ID = strconv.Itoa(rand.Intn(100000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	// set json content type
+	w.Header().Set("Content-Type", "application/json")
+	// params
+	params := mux.Vars(r)
+	// loop over the movies, range
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			// delete the movie with i.d
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
+
+	// add a new movie
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID: "1", Isbm: "38227", Title: "Movie One", Director: &Director{"John", "Doe"}})
+	movies = append(movies, Movie{ID: "2", Isbm: "45455", Title: "Movie Two", Director: &Director{"Steve", "Smith"}})
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movie/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movie", createMovie).Methods("POST")
+	r.HandleFunc("/movie/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movie/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Println("Start on port :8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-sandbox
 
 go 1.18
+
+require github.com/gorilla/mux v1.8.0
